bb4a54a48f131e86e3050427a66989fbbb889e27
soosgabor <malagi@gmail.com>
2023-04-22 18:58:51 +0200

    cors mod

--- server.js
+++ server.js
@@ -64,6 +64,13 @@ app.use(hpp())
 // CORS engedélyezése
 app.use(cors())
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
+})
+
 app.use(express.static(path.join(__dirname, 'public/uploads')))
 
 app.use("/api/trainings", trainings);
